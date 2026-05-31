aee7859efb8e338a27dfdcc54ea7f00c25d2ccf9
klingenbo <lina.klingberg@gmail.com>
2026-05-31 09:32:41 +0200

    feat: add navigation for onboarding screens

--- app/src/main/java/com/example/whatsfordinner/MainActivity.kt
+++ app/src/main/java/com/example/whatsfordinner/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.whatsfordinner.ui.theme.WhatsForDinnerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WhatsForDinnerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "onboarding1"
+                ) {
+
+                    composable("onboarding1") {
+                        Onboarding1(
+                            onContinueClicked = {
+                                navController.navigate("onboarding2")
+                            }
+                        )
+                    }
+
+                    composable("onboarding2") {
+                        Onboarding2(
+                            onContinueClicked = {
+                                navController.navigate("onboarding3")
+                            }
+                        )
+                    }
+
+                    composable("onboarding3") {
+                        Onboarding3()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WhatsForDinnerTheme {
-        Greeting("Android")
     }
 }
