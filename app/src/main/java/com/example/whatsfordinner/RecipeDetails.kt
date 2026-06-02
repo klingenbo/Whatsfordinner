6e2dc61afaf16999439df088e4c027c204da8d89
klingenbo <lina.klingberg@gmail.com>
2026-06-02 09:24:01 +0200

    prep: add recipe details screen

---
+++ app/src/main/java/com/example/whatsfordinner/RecipeDetails.kt
@@ -0,0 +1,15 @@
+package com.example.whatsfordinner
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeDetails() {
+
+    Column() {
+        Text(
+            text = "Recipe Details"
+        )
+    }
+}
